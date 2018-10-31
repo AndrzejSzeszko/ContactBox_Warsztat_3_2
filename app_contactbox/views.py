3b94754b5bf70a30bda197b18c8c550bfe937eb7
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-11-01 00:34:03 +0100

    create basic HomeView

--- app_contactbox/views.py
+++ app_contactbox/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import (render,
+                              redirect)
+from django.views import View
+from .models import (Person,
+                     Address,
+                     Group,
+                     Phone,
+                     Email)
+from .forms import (PersonForm,
+                    AddressForm,
+                    PhoneForm,
+                    EmailForm,
+                    GroupForm)
 
-# Create your views here.
+
+class HomeView(View):
+
+    def get(self, request):
+        persons = Person.objects.all().order_by('surname', 'name')
+        return render(request, 'app_contactbox/home.html', {'persons': persons})
