1d22ce9850c2a61ef34788af8966099b1e464455
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-11-01 00:32:39 +0100

    change IntegerField to CharField in PhoneForm

--- app_contactbox/forms.py
+++ app_contactbox/forms.py
@@ -7,7 +7,7 @@ from .models import (Person,
                      Email)
 
 
-class NewPersonForm(forms.Form):
+class PersonForm(forms.Form):
     name = forms.CharField(label='Name:', max_length=64)
     surname = forms.CharField(label='Surname:', max_length=64, empty_value=None, required=False)
     description = forms.CharField(label='Description:', max_length=256, empty_value=None, required=False)
@@ -16,24 +16,24 @@ class NewPersonForm(forms.Form):
     groups = forms.ModelMultipleChoiceField(label='Groups:', queryset=Group.objects.all(), required=False)
 
 
-class NewAddressForm(forms.Form):
+class AddressForm(forms.Form):
     town = forms.CharField(label='Town:', max_length=64)
     street = forms.CharField(label='Street:', max_length=64, empty_value=None, required=False)
     house_no = forms.CharField(label='House No:', max_length=8, empty_value=None, required=False)
     apartment_no = forms.CharField(label='Apartment No:', max_length=8, empty_value=None, required=False)
     
 
-class NewPhoneForm(forms.Form):
-    number = forms.IntegerField(label='Number:', max_length=9)
+class PhoneForm(forms.Form):
+    number = forms.CharField(label='Number:', max_length=9)
     phone_type = forms.ChoiceField(label='Phone type:', choices=Phone.PHONE_TYPES, empty_value=None, required=False)
     person = forms.ModelChoiceField(label='Person:', queryset=Person.objects.all())
 
 
-class NewEmailForm(forms.Form):
+class EmailForm(forms.Form):
     email = forms.CharField(label='Email:', max_length=64)
     email_type = forms.ChoiceField(label='Email type:', choices=Email.EMAIL_TYPES, empty_value=None, required=False)
     person = forms.ModelChoiceField(label='Person:', queryset=Person.objects.all())
 
 
-class NewGroupForm(forms.Form):
+class GroupForm(forms.Form):
     group_name = forms.ChoiceField(label='Name:', max_length=32)
