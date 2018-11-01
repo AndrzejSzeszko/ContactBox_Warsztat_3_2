d0bace3bccc90f48e0c6d8da305e7960daf3c015
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-11-01 03:05:07 +0100

    create "post" method for NewPersonView

--- app_contactbox/views.py
+++ app_contactbox/views.py
@@ -25,3 +25,16 @@ class NewPersonView(View):
     def get(self, request):
         person_form = PersonForm()
         return render(request, 'app_contactbox/new_person.html', {'person_form': person_form})
+    
+    def post(self, request):
+        
+        data = {
+            'name': request.POST.get('name'),
+            'surname': request.POST.get('surname'),
+            'description': request.POST.get('description'),
+            'address': Address.objects.filter(id=request.POST.get('address') if request.POST.get('address') else None).first(),
+        }
+
+        Person.objects.create(**data).groups.set(request.POST.get('groups') if request.POST.get('groups') else [])
+        
+        return redirect('new_person')
