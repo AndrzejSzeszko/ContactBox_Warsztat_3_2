e51fba072274a263eeb54ad9f54cc7f02e692230
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-11-01 01:30:57 +0100

    add basic NewPersonView

--- app_contactbox/views.py
+++ app_contactbox/views.py
@@ -18,3 +18,10 @@ class HomeView(View):
     def get(self, request):
         persons = Person.objects.all().order_by('surname', 'name')
         return render(request, 'app_contactbox/home.html', {'persons': persons})
+
+
+class NewPersonView(View):
+
+    def get(self, request):
+        person_form = PersonForm()
+        return render(request, 'app_contactbox/new_person.html', {'person_form': person_form})
