0caa1411327d511e59ea2a46287494deb73ae40a
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-11-01 22:30:35 +0100

    create "new-address" and "new-address-info" paths

--- app_contactbox/urls.py
+++ app_contactbox/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from app_contactbox import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('new-person/', views.NewPersonView.as_view(), name='new_person'),
+    path('', views.AllContactsView.as_view(), name='all-contacts'),
+    path('new-person/', views.NewPersonView.as_view(), name='new-person'),
+    path('new-address/', views.NewAddressView.as_view(), name='new-address'),
+    path('new-address/<str:info>/', views.NewAddressView.as_view(), name='new-address-info'),
 ]
