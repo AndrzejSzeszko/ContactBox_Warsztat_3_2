51c638e0ab04d71dbc52a339629c11b83ba1091f
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-11-01 01:30:31 +0100

    add path for NewPersonView

--- app_contactbox/urls.py
+++ app_contactbox/urls.py
@@ -4,4 +4,5 @@ from app_contactbox import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('new-person/', views.NewPersonView.as_view(), name='new_person'),
 ]
