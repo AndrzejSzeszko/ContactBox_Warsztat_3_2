3d8193b153b65038f8f29928dfa43b6d7e0cd7bc
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-11-02 04:10:57 +0100

    add new-group(-info) and new-phone(-info) paths

--- app_contactbox/urls.py
+++ app_contactbox/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('new-person/<str:info>/', views.NewPersonView.as_view(), name='new-person-info'),
     path('new-address/', views.NewAddressView.as_view(), name='new-address'),
     path('new-address/<str:info>/', views.NewAddressView.as_view(), name='new-address-info'),
+    path('new-group/', views.NewGroupView.as_view(), name='new-group'),
+    path('new-group/<str:info>', views.NewGroupView.as_view(), name='new-group-info'),
+    path('new-phone/', views.NewPhoneView.as_view(), name='new-phone'),
+    path('new-phone/<str:info>', views.NewPhoneView.as_view(), name='new-phone-info'),
 ]
